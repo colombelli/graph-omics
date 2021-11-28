c9b3895e67532c7dc191d488e63e789bc3d61721
Felipe Colombelli <fcolombelli@inf.ufrgs.br>
2021-11-28 15:12:55 -0300

    Add top10 genes stage query

--- query_database/GraphOmicsDB.py
+++ query_database/GraphOmicsDB.py
@@ -65,16 +65,56 @@ class GraphOmicsDB:
         values = [record.data() for record in result]
         return values
         
-    def tumoral_10_least_expressed_genes(self):
+    def normal_10_least_expressed_genes(self):
         with self.driver.session() as session:
-            values = session.read_transaction(self._tumoral_10_least_expressed_genes)
+            values = session.read_transaction(self._normal_10_least_expressed_genes)
         return values
     @staticmethod
-    def _tumoral_10_least_expressed_genes(tx):
+    def _normal_10_least_expressed_genes(tx):
         query = """MATCH (:Patient) -[ne:HAS_NORMAL_EXPRESSION_OF] -> (g:Gene)
         RETURN g AS gene, avg(toFloat(ne.expression)) AS normal_expression_avg
         ORDER BY normal_expression_avg
         LIMIT 10"""
+        result = tx.run(query)
+        values = [record.data() for record in result]
+        return values
+
+
+    def by_stage_10_most_expressed_genes(self):
+        global stage
+        result = {}
+        with self.driver.session() as session:
+            for s in ["stage i", "stage ii", "stage iii", "stage iv"]:
+                stage = s
+                result[stage] = session.read_transaction(self._stage_10_most_expressed_genes)
+        return result
+    @staticmethod
+    def _stage_10_most_expressed_genes(tx):
+        query = """MATCH (p:Patient) -[te:HAS_TUMORAL_EXPRESSION_OF] -> (g:Gene)
+        WHERE p.pathologic_stage = "%s"
+        RETURN g AS gene, avg(toFloat(te.expression)) AS tumoral_expression_avg
+        ORDER BY tumoral_expression_avg DESC
+        LIMIT 10""" % stage
+        result = tx.run(query)
+        values = [record.data() for record in result]
+        return values
+
+
+    def by_stage_10_least_expressed_genes(self):
+        global stage
+        result = {}
+        with self.driver.session() as session:
+            for s in ["stage i", "stage ii", "stage iii", "stage iv"]:
+                stage = s
+                result[stage] = session.read_transaction(self._stage_10_least_expressed_genes)
+        return result
+    @staticmethod
+    def _stage_10_least_expressed_genes(tx):
+        query = """MATCH (p:Patient) -[te:HAS_TUMORAL_EXPRESSION_OF] -> (g:Gene)
+        WHERE p.pathologic_stage = "%s"
+        RETURN g AS gene, avg(toFloat(te.expression)) AS tumoral_expression_avg
+        ORDER BY tumoral_expression_avg
+        LIMIT 10""" % stage
         result = tx.run(query)
         values = [record.data() for record in result]
         return values
--- query_database/test.py
+++ query_database/test.py
@@ -2,6 +2,6 @@ from GraphOmicsDB import GraphOmicsDB
 
 if __name__ == "__main__":
     app = GraphOmicsDB("bolt://localhost:7687", "neo4j", "qwaszx123")
-    r = app.tumoral_10_least_expressed_genes()
+    r = app.by_stage_10_least_expressed_genes()
     print("results:", r)
     app.close()
