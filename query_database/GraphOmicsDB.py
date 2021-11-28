6710ab55d71473079c1aee0a2acd3cd1c6cfcf74
Felipe Colombelli <fcolombelli@inf.ufrgs.br>
2021-11-28 14:34:28 -0300

    Integrate ciça queries

--- query_database/GraphOmicsDB.py
+++ query_database/GraphOmicsDB.py
@@ -12,7 +12,6 @@ class GraphOmicsDB:
         with self.driver.session() as session:
             values, info = session.read_transaction(self._count_genes)
         return values, info
-
     @staticmethod
     def _count_genes(tx):
         query = "MATCH (g:Gene) RETURN COUNT(g)"
@@ -21,3 +20,19 @@ class GraphOmicsDB:
         value = record.value()
         info = result.consume()
         return value, info
+
+
+    def tumoral_10_most_expressed_genes(self):
+        with self.driver.session() as session:
+            values = session.read_transaction(self._tumoral_10_most_expressed_genes)
+        return values
+    @staticmethod
+    def _tumoral_10_most_expressed_genes(tx):
+        query = """MATCH (:Patient) -[te:HAS_TUMORAL_EXPRESSION_OF] -> (g:Gene)
+        RETURN g AS gene, avg(toFloat(te.expression)) AS tumoral_expression_avg
+        ORDER BY tumoral_expression_avg DESC
+        LIMIT 10"""
+        result = tx.run(query)
+        values = [record.data() for record in result]
+        return values
+        
--- query_database/test.py
+++ query_database/test.py
@@ -2,6 +2,6 @@ from GraphOmicsDB import GraphOmicsDB
 
 if __name__ == "__main__":
     app = GraphOmicsDB("bolt://localhost:7687", "neo4j", "qwaszx123")
-    c = app.count_genes()
-    print("Count:", c)
+    r = app.tumoral_10_most_expressed_genes()
+    print("results:", r)
     app.close()
